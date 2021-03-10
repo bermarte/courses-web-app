8fba76186fb72762a9b86e8c38ffa1b492132a99
KrystynaMil <kristina-manager@hotmail.com>
2021-03-10 11:44:15 +0100

    added controllers.js and routes.js

--- api/routes.js
+++ api/routes.js
@@ -4,8 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', controllers.hello);
+router.use((req, res, next) => {
+    console.log('routes!');
+    next();
+});
 
 // write your routes
-
+router.get('/courses', controllers.getAllCourses);
+router.get('/courses/:id', controllers.getCourseId);
+router.post('/courses', controllers.saveCourse);
+router.put('/courses/:id', controllers.editFile);
+router.delete("/courses/:id", controllers.deleteCourse);
 
 module.exports = router;
