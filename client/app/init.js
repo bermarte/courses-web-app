67c90c503a570dfa497a6938988ccfa5b539c928
Bermarte <bernardo.martelli@gmail.com>
2021-03-11 14:16:43 +0100

    starting refactoring

--- client/app/init.js
+++ client/app/init.js
@@ -3,3 +3,19 @@ export const init = async () => {
   const data = await res.json();
   console.log(data);
 };
+
+
+
+//get data from form
+//do data validation
+const newCourse = document.getElementById('newCourse');
+newCourse.addEventListener('click', (event) => {
+  event.preventDefault();
+  const newData = {
+    name: document.getElementById("name").value,
+    location: document.getElementById("location").value,
+    details: document.getElementById("details").value
+  }
+  console.log(newData);
+
+});
