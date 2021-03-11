66a62497f7103a03d3e132995ff457d953e33dd9
KrystynaMil <kristina-manager@hotmail.com>
2021-03-11 11:52:26 +0100

    some modifications

--- api/routes.js
+++ api/routes.js
@@ -4,16 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', controllers.hello);
-router.use((req, res, next) => {
-    console.log('routes!');
-    next();
-});
+
 
 // write your routes
 router.get('/courses', controllers.getAllCourses);
 router.get('/courses/:id', controllers.getCourseId);
 router.post('/courses', controllers.saveCourse);
-router.put('/courses/:id', controllers.editFile);
+router.put('/courses/:id', controllers.editCourse);
+router.get('/courses', controllers.listCourses);
 router.delete("/courses/:id", controllers.deleteCourse);
 
 module.exports = router;
